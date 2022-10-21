48555aaf6f615a4b341b969494b2e36622fcaf36
Seamus Lowry <spl1owry3@gmail.com>
2022-10-21 17:43:09 -0400

    changes

--- auth/user.py
+++ auth/user.py
@@ -2,22 +2,28 @@ import azure.functions as func
 import base64
 import json
 
+
 def id(req: func.HttpRequest):
-  return req.headers.get("x-ms-client-principal-id")
+    return req.headers.get("x-ms-client-principal-id")
+
 
 def identifier(req: func.HttpRequest):
-  return req.headers.get("x-ms-client-principal-name")
+    return req.headers.get("x-ms-client-principal-name")
+
 
 def name(req: func.HttpRequest):
-  return __get_claim(req, "name")
+    return __get_claim(req, "name")
+
 
 def picture_url(req: func.HttpRequest):
-  return __get_claim(req, "picture")
+    return __get_claim(req, "picture")
+
 
 def __get_claims(req: func.HttpRequest):
-  token = req.headers.get("x-ms-client-principal")
-  return json.loads(base64.b64decode(token).decode('utf-8'))['claims'] if token else []
+    token = req.headers.get("x-ms-client-principal")
+    return json.loads(base64.b64decode(token).decode('utf-8'))['claims'] if token else []
+
 
 def __get_claim(req: func.HttpRequest, claim: str):
-  claims = __get_claims(req)
-  return next((x for x in claims if x['typ'] == claim), {'val': None})['val']
+    claims = __get_claims(req)
+    return next((x for x in claims if x['typ'] == claim), {'val': None})['val']
