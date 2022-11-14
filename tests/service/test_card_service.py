9796ebf8708e29320bf2941474d35eb02cddec01
Seamus Lowry <spl1owry3@gmail.com>
2022-11-14 17:52:23 -0500

    test card better

--- tests/service/test_card_service.py
+++ tests/service/test_card_service.py
@@ -11,17 +11,16 @@ from service import card
 class TestCardService(TestCase):
     '''Unit tests to ensure card translations work as expected'''
 
-    def test_card_to_db(self):
-        '''Card can be converted for DB save'''
-        self.assertIsNotNone(card.to_db(
-            Card(CardNumber.ACE, SelectableSuit.CLUBS)))
+    def test_selectable_suit_card_conversion(self):
+        '''Selectable suit card can be converted to and from a DB save'''
+        initial_card = Card(CardNumber.ACE, SelectableSuit.CLUBS)
 
-    def test_selectable_suit_card_from_db(self):
-        '''Selectable suit card can be converted from DB save'''
-        self.assertIsNotNone(card.from_db(
-            card.to_db(Card(CardNumber.ACE, SelectableSuit.CLUBS))))
+        self.assertIsNotNone(initial_card, card.from_db(
+            card.to_db(initial_card)))
 
     def test_unselectable_suit_card_from_db(self):
         '''Unselectable suit card can be converted from DB save'''
-        self.assertIsNotNone(card.from_db(
-            card.to_db(Card(CardNumber.JOKER, UnselectableSuit.JOKER))))
+        initial_card = Card(CardNumber.JOKER, UnselectableSuit.JOKER)
+
+        self.assertIsNotNone(initial_card, card.from_db(
+            card.to_db(initial_card)))
