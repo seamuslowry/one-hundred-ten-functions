96f2fafc31d5d7ddb456f91f1018137e36f1105c
Seamus Lowry <spl1owry3@gmail.com>
2022-11-17 07:24:06 -0500

    mock fix

--- tests/functions/test_create_game.py
+++ tests/functions/test_create_game.py
@@ -10,7 +10,7 @@ from tests.helpers import build_request, read_response_body
 class TestCreateGame(TestCase):
     '''Create Game unit tests'''
 
-    @mock.patch('service.GameService.save', return_value={})
+    @mock.patch('services.GameService.save', return_value={})
     def test_creates_game(self, save):
         '''On hitting the create request a game is created and returned'''
         req = build_request()
