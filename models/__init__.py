c30bb0791f2b6e336b06b0645c3cff2587cd09e2
Seamus Lowry <spl1owry3@gmail.com>
2022-11-18 07:29:32 -0500

    add config to fix error

--- models/__init__.py
+++ models/__init__.py
@@ -1,24 +1,14 @@
 '''Init the models module'''
+
+from hundredandten.actions import Bid, Discard, Play, SelectTrump, Unpass
+from hundredandten.constants import (Accessibility, BidAmount, CardNumber,
+                                     GameRole, GameStatus, RoundRole,
+                                     RoundStatus, SelectableSuit,
+                                     UnselectableSuit)
+from hundredandten.deck import Card, Deck
+from hundredandten.group import Group, Person, Player
+from hundredandten.round import Round
+from hundredandten.trick import Trick
+
 from models.game import Game
-from models.passthroughs import ServerlessAccessibility as Accessibility
-from models.passthroughs import ServerlessBid as Bid
-from models.passthroughs import ServerlessBidAmount as BidAmount
-from models.passthroughs import ServerlessCard as Card
-from models.passthroughs import ServerlessCardNumber as CardNumber
-from models.passthroughs import ServerlessDeck as Deck
-from models.passthroughs import ServerlessDiscard as Discard
-from models.passthroughs import ServerlessGameRole as GameRole
-from models.passthroughs import ServerlessGameStatus as GameStatus
-from models.passthroughs import ServerlessGroup as Group
-from models.passthroughs import ServerlessPerson as Person
-from models.passthroughs import ServerlessPlay as Play
-from models.passthroughs import ServerlessPlayer as Player
-from models.passthroughs import ServerlessRound as Round
-from models.passthroughs import ServerlessRoundRole as RoundRole
-from models.passthroughs import ServerlessRoundStatus as RoundStatus
-from models.passthroughs import ServerlessSelectableSuit as SelectableSuit
-from models.passthroughs import ServerlessSelectTrump as SelectTrump
-from models.passthroughs import ServerlessTrick as Trick
-from models.passthroughs import ServerlessUnpass as Unpass
-from models.passthroughs import ServerlessUnselectableSuit as UnselectableSuit
 from models.user import GoogleUser, User
