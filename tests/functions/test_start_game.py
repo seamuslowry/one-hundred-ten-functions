7f79b989b7765561c90127b3b649262d2e1a5d57
Seamus Lowry <spl1owry3@gmail.com>
2022-11-19 17:45:36 -0500

    TDD

---
+++ tests/functions/test_start_game.py
@@ -0,0 +1,56 @@
+'''Start game unit tests'''
+import json
+from unittest import TestCase, mock
+
+from models import Game, GameRole, Group, Person
+from start_game import main
+from tests.helpers import (DEFAULT_ID, DEFAULT_USER, build_request,
+                           read_response_body, return_input)
+
+
+class TestStartGame(TestCase):
+    '''Start Game unit tests'''
+
+    @mock.patch('services.GameService.save', side_effect=return_input)
+    @mock.patch('services.UserService.save', mock.Mock(side_effect=return_input))
+    @mock.patch(
+        'services.GameService.get', mock.Mock(
+            return_value=Game(
+                people=Group(
+                    [Person(DEFAULT_ID, roles={GameRole.PLAYER}),
+                     Person(DEFAULT_ID + '2', roles={GameRole.PLAYER})]))))
+    @mock.patch('services.UserService.get', mock.Mock(return_value=DEFAULT_USER))
+    def test_starts_game_if_organizer(self, game_save):
+        '''When the organizer hits the start endpoint the game begins'''
+        invitee = 'invitee'
+        req = build_request(
+            route_params={'id': 'id'},
+            body=json.dumps({'invitees': [invitee]}).encode('utf-8'))
+
+        resp = main(req)
+        resp_dict = read_response_body(resp.get_body())
+
+        game_save.assert_called_once()
+        self.assertIn('round', resp_dict)
+
+    @mock.patch('services.GameService.save', side_effect=return_input)
+    @mock.patch('services.UserService.save', mock.Mock(side_effect=return_input))
+    @mock.patch(
+        'services.GameService.get', mock.Mock(
+            return_value=Game(
+                people=Group(
+                    [Person(DEFAULT_ID + '1', roles={GameRole.PLAYER}),
+                     Person(DEFAULT_ID + '2', roles={GameRole.PLAYER})]))))
+    @mock.patch('services.UserService.get', mock.Mock(return_value=DEFAULT_USER))
+    def test_starts_game_if_not_organizer(self, game_save):
+        '''When someone not organizer hits the start endpoint, nothing happens'''
+        invitee = 'invitee'
+        req = build_request(
+            route_params={'id': 'id'},
+            body=json.dumps({'invitees': [invitee]}).encode('utf-8'))
+
+        resp = main(req)
+        resp_dict = read_response_body(resp.get_body())
+
+        game_save.assert_called_once()
+        self.assertNotIn('round', resp_dict)
