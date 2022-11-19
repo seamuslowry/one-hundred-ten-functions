58b2f5bfcc2695e58370243977cd991c9b781e09
Seamus Lowry <spl1owry3@gmail.com>
2022-11-19 17:54:18 -0500

    get game

---
+++ game_info/__init__.py
@@ -0,0 +1,20 @@
+'''
+Endpoint to retrieve a game
+'''
+import json
+
+import azure.functions as func
+
+from decorators import catcher
+from services import GameService, UserService
+
+
+@catcher
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    '''
+    Retrieve 110 game.
+    '''
+    user = UserService.from_request(req)
+    game = GameService.get(req.route_params['id'])
+
+    return func.HttpResponse(json.dumps(GameService.json(game, user.identifier)))
