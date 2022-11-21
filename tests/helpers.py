6afed1b80ba1d801ec1ef2655d85164e79df10aa
Seamus Lowry <spl1owry3@gmail.com>
2022-11-21 16:25:20 -0500

    TDD

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,10 +1,12 @@
 '''Helpers to perform common functions during testing'''
 import json
-from typing import Optional
+from typing import Optional, Union
 
 import azure.functions as func
 
-from models import User
+from models import (Bid, BidAmount, Discard, GameRole, GameStatus, Group,
+                    Person, RoundStatus, SelectableSuit, SelectTrump, User)
+from models.game import Game
 
 DEFAULT_ID = 'id'
 DEFAULT_NAME = 'name'
@@ -31,3 +33,72 @@ def read_response_body(body: bytes) -> dict:
 def return_input(param):
     '''Return the parameter; useful for mocks'''
     return param
+
+
+def game(
+        status: Union[GameStatus, RoundStatus]) -> Game:
+    '''
+    Return a game in the requested status.
+    '''
+
+    new_game = {
+        GameStatus.WAITING_FOR_PLAYERS: __get_waiting_for_players_game,
+        RoundStatus.BIDDING: __get_bidding_game,
+        RoundStatus.COMPLETED_NO_BIDDERS: __get_completed_no_bidders_game,
+        RoundStatus.TRUMP_SELECTION: __get_trump_selection_game,
+        RoundStatus.DISCARD: __get_discard_game,
+        RoundStatus.TRICKS: __get_tricks_game,
+    }[status]()
+    return new_game
+
+
+def __get_waiting_for_players_game() -> Game:
+    '''Returns a game that is waiting for players'''
+    new_game = Game(
+        people=Group(
+            list(map(
+                lambda identifier: Person(str(identifier), roles={GameRole.PLAYER}),
+                ['0', DEFAULT_ID, *range(1, 3)]))))
+    new_game.people.add_role(new_game.people[0].identifier, GameRole.ORGANIZER)
+    return new_game
+
+
+def __get_bidding_game() -> Game:
+    '''Returns a game in the bidding status'''
+    new_game = __get_waiting_for_players_game()
+    new_game.start_game()
+    return new_game
+
+
+def __get_completed_no_bidders_game() -> Game:
+    '''Returns a game in the completed no bidders status'''
+    new_game = __get_bidding_game()
+    new_game.active_round.bids = [Bid(p.identifier, BidAmount.PASS) for p in new_game.players]
+    return new_game
+
+
+def __get_trump_selection_game() -> Game:
+    '''Return a game in the trump selection status'''
+    new_game = __get_bidding_game()
+    new_game.active_round.bids = [Bid(p.identifier, BidAmount.PASS)
+                                  for p in new_game.active_round.inactive_players] + [
+        Bid(new_game.active_round.active_player.identifier, BidAmount.FIFTEEN)]
+    return new_game
+
+
+def __get_discard_game() -> Game:
+    '''Return a game in the discard status'''
+    new_game = __get_trump_selection_game()
+    active_bidder = new_game.active_round.active_bidder
+    assert active_bidder
+    new_game.active_round.selection = SelectTrump(
+        active_bidder.identifier, SelectableSuit.DIAMONDS)
+    return new_game
+
+
+def __get_tricks_game() -> Game:
+    '''Return a game in the tricks status'''
+    new_game = __get_discard_game()
+    new_game.active_round.discards = [Discard(p.identifier, []) for p in new_game.players]
+
+    return new_game
