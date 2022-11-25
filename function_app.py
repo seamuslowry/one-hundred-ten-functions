537c2bdc86b929c7d284c11a52e21cc08b0b2ffa
Seamus Lowry <spl1owry3@gmail.com>
2022-11-24 20:02:45 -0500

    initial test

---
+++ function_app.py
@@ -0,0 +1,16 @@
+'''Base Function App file'''
+import azure.functions as func
+
+from services import UserService
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+
+@app.function_name(name="TestRoute")
+@app.route(route="test", methods=[func.HttpMethod.GET])
+def test_function(req: func.HttpRequest) -> func.HttpResponse:
+    '''Test Function'''
+    user = UserService.from_request(req)
+
+    return func.HttpResponse(
+        f"Hello, {user.name}. This HTTP triggered function executed successfully.")
