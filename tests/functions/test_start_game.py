27bf2125f6f34bcef87a8893ebf54d42d9d94f75
Seamus Lowry <spl1owry3@gmail.com>
2022-11-28 22:03:15 -0500

    start game

--- tests/functions/test_start_game.py
+++ tests/functions/test_start_game.py
@@ -3,21 +3,19 @@ from unittest import TestCase, mock
 
 from app.models import Game, GameRole, Group, Person
 from start_game import main
-from tests.helpers import (DEFAULT_ID, build_request, read_response_body,
-                           return_input)
+from tests.helpers import (DEFAULT_ID, DEFAULT_USER, build_request,
+                           read_response_body, return_input)
 
 
 class TestStartGame(TestCase):
     '''Start Game unit tests'''
 
     @mock.patch('app.services.GameService.save', side_effect=return_input)
-    @mock.patch('app.services.UserService.save', mock.Mock(side_effect=return_input))
-    @mock.patch(
-        'app.services.GameService.get', mock.Mock(
-            return_value=Game(
-                people=Group(
-                    [Person(DEFAULT_ID, roles={GameRole.PLAYER}),
-                     Person(DEFAULT_ID + '2', roles={GameRole.PLAYER})]))))
+    @mock.patch('start_game.parse_request',
+                mock.Mock(return_value=(DEFAULT_USER, Game(
+                    people=Group(
+                        [Person(DEFAULT_ID, roles={GameRole.PLAYER}),
+                         Person(DEFAULT_ID + '2', roles={GameRole.PLAYER})])), 0)))
     def test_starts_game_if_organizer(self, game_save):
         '''When the organizer hits the start endpoint the game begins'''
         req = build_request(route_params={'id': 'id'})
@@ -29,13 +27,11 @@ class TestStartGame(TestCase):
         self.assertIn('round', resp_dict)
 
     @mock.patch('app.services.GameService.save', side_effect=return_input)
-    @mock.patch('app.services.UserService.save', mock.Mock(side_effect=return_input))
-    @mock.patch(
-        'app.services.GameService.get', mock.Mock(
-            return_value=Game(
-                people=Group(
-                    [Person(DEFAULT_ID + '1', roles={GameRole.PLAYER}),
-                     Person(DEFAULT_ID + '2', roles={GameRole.PLAYER})]))))
+    @mock.patch('start_game.parse_request',
+                mock.Mock(return_value=(DEFAULT_USER, Game(
+                    people=Group(
+                        [Person(DEFAULT_ID + '1', roles={GameRole.PLAYER}),
+                         Person(DEFAULT_ID + '2', roles={GameRole.PLAYER})])), 0)))
     def test_starts_game_if_not_organizer(self, game_save):
         '''When someone not organizer hits the start endpoint, nothing happens'''
         req = build_request(route_params={'id': 'id'})
