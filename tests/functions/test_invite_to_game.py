5b6283c12bbfd275ba5cbf664427d7bedea78773
Seamus Lowry <spl1owry3@gmail.com>
2022-11-28 21:36:31 -0500

    invite

--- tests/functions/test_invite_to_game.py
+++ tests/functions/test_invite_to_game.py
@@ -4,17 +4,19 @@ from unittest import TestCase, mock
 
 from app.models import Game, GameRole, Group, Person
 from invite_to_game import main
-from tests.helpers import (DEFAULT_ID, build_request, read_response_body,
-                           return_input)
+from tests.helpers import (DEFAULT_ID, DEFAULT_USER, build_request,
+                           read_response_body, return_input)
 
 
 class TestInviteToGame(TestCase):
     '''Invite to Game unit tests'''
 
     @mock.patch('app.services.GameService.save', side_effect=return_input)
-    @mock.patch('app.services.UserService.save', mock.Mock(side_effect=return_input))
-    @mock.patch('app.services.GameService.get', mock.Mock(
-        return_value=Game(people=Group([Person(DEFAULT_ID, roles={GameRole.PLAYER})]))))
+    @mock.patch(
+        'invite_to_game.parse_request', mock.Mock(
+            return_value=(DEFAULT_USER,
+                          Game(people=Group([Person(DEFAULT_ID, roles={GameRole.PLAYER})])),
+                          0)))
     def test_invites_game(self, game_save):
         '''On hitting the invite endpoint the logged in player invites the listed users'''
         invitee = 'invitee'
