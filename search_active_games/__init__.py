91cd18341734e01fe2adfc692e22bc120089595d
Seamus Lowry <spl1owry3@gmail.com>
2022-12-03 09:23:45 -0500

    initial endpoint implementation

---
+++ search_active_games/__init__.py
@@ -0,0 +1,30 @@
+'''
+Endpoint to get active games from the DB
+'''
+import json
+
+import azure.functions as func
+
+from app.decorators import catcher
+from app.models import GameRole
+from app.parsers import parse_request
+from app.services import GameService
+
+
+@catcher
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    '''
+    Get active games
+    '''
+    user, *_ = parse_request(req)
+
+    body = req.get_json()
+    max_count = body.get('max', 20)
+    search_text = body.get('searchText', '')
+
+    return func.HttpResponse(
+        json.dumps(
+            list(
+                map(
+                    lambda g: GameService.json(g, user.identifier),
+                    GameService.search_active(search_text, max_count, user.identifier)))))
