e45c26080166e078c04238034785bd2673bab13a
Seamus Lowry <spl1owry3@gmail.com>
2022-12-31 16:30:55 -0500

    delete event service

--- tests/services/test_event_service.py
+++ tests/services/test_event_service.py
@@ -4,7 +4,7 @@ from unittest import TestCase
 from app.models import (Bid, BidAmount, Card, CardNumber, Discard, GameEnd,
                         GameStart, Play, RoundEnd, RoundStart, SelectableSuit,
                         SelectTrump, TrickEnd, TrickStart)
-from app.services import CardService, EventService
+from app.services import CardService
 
 card = Card(CardNumber.ACE, SelectableSuit.CLUBS)
 
@@ -30,25 +30,26 @@ def index_of(event_type: type) -> int:
 class TestEventService(TestCase):
     '''Event service unit tests'''
 
-    def test_event_conversion_without_details(self):
-        '''Events can be converted when no one matches the identifier'''
-        json_events = EventService.json(raw_events, 'noMatch')
+    # TODO Fix
+    # def test_event_conversion_without_details(self):
+    #     '''Events can be converted when no one matches the identifier'''
+    #     json_events = EventService.json(raw_events, 'noMatch')
 
-        round_start_index = index_of(RoundStart)
-        discard_index = index_of(Discard)
+    #     round_start_index = index_of(RoundStart)
+    #     discard_index = index_of(Discard)
 
-        self.assertEqual(len(raw_events), len(json_events))
-        self.assertEqual(1, json_events[round_start_index]['hands']['1'])
-        self.assertEqual(1, json_events[discard_index]['discards'])
+    #     self.assertEqual(len(raw_events), len(json_events))
+    #     self.assertEqual(1, json_events[round_start_index]['hands']['1'])
+    #     self.assertEqual(1, json_events[discard_index]['discards'])
 
-    def test_event_conversion_with_details(self):
-        '''Events can be converted when the client matches the identifier'''
-        bid_index = index_of(Bid)
-        json_events = EventService.json(raw_events, raw_events[bid_index].identifier)
+    # def test_event_conversion_with_details(self):
+    #     '''Events can be converted when the client matches the identifier'''
+    #     bid_index = index_of(Bid)
+    #     json_events = EventService.json(raw_events, raw_events[bid_index].identifier)
 
-        round_start_index = index_of(RoundStart)
-        discard_index = index_of(Discard)
+    #     round_start_index = index_of(RoundStart)
+    #     discard_index = index_of(Discard)
 
-        self.assertEqual(len(raw_events), len(json_events))
-        self.assertEqual([CardService.json(card)], json_events[round_start_index]['hands']['1'])
-        self.assertEqual([CardService.json(card)], json_events[discard_index]['discards'])
+    #     self.assertEqual(len(raw_events), len(json_events))
+    #     self.assertEqual([CardService.json(card)], json_events[round_start_index]['hands']['1'])
+    #     self.assertEqual([CardService.json(card)], json_events[discard_index]['discards'])
