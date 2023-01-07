bb84a2b903857a245db0918cb21f0b40eb18b1dc
Seamus Lowry <spl1owry3@gmail.com>
2023-01-07 09:07:00 -0500

    joining tests

--- tests/functions/test_lobby_game.py
+++ tests/functions/test_lobby_game.py
@@ -3,6 +3,7 @@ from unittest import TestCase
 
 import create_game
 import invite_to_game
+import join_game
 from app.dtos.client import WaitingGame
 from app.models import GameStatus
 from tests.helpers import build_request, read_response_body
@@ -31,7 +32,7 @@ class TestLobbyGame(TestCase):
 
         resp = create_game.main(
             build_request(
-                body={'name': 'invite test'}))
+                body={'name': 'organizer invite test'}))
         created_game: WaitingGame = read_response_body(resp.get_body())
 
         resp = invite_to_game.main(
@@ -54,7 +55,7 @@ class TestLobbyGame(TestCase):
 
         resp = create_game.main(
             build_request(
-                body={'name': 'invite test'}))
+                body={'name': 'invitee invite test'}))
         created_game: WaitingGame = read_response_body(resp.get_body())
 
         # invite the original
@@ -71,3 +72,98 @@ class TestLobbyGame(TestCase):
                 headers={'x-ms-client-principal-id': invitee},
                 body={'invitees': [second_invitee]}))
         self.assertEqual(400, failed_invite.status_code)
+
+    def test_player_invite_to_game(self):
+        '''Players can invite other players to a game'''
+        invitee = 'invitee'
+        player = 'player'
+
+        resp = create_game.main(
+            build_request(
+                body={'name': 'player invite test'}))
+        created_game: WaitingGame = read_response_body(resp.get_body())
+
+        # join as player
+        resp = join_game.main(
+            build_request(
+                route_params={'game_id': created_game['id']},
+                headers={'x-ms-client-principal-id': player}))
+
+        # new player can invite
+        invite = invite_to_game.main(
+            build_request(
+                route_params={'game_id': created_game['id']},
+                headers={'x-ms-client-principal-id': player},
+                body={'invitees': [invitee]}))
+        invited_game: WaitingGame = read_response_body(invite.get_body())
+
+        self.assertEqual(created_game['id'], invited_game['id'])
+        self.assertEqual(1, len(invited_game['players']))
+        self.assertEqual(player, invited_game['players'][0]['identifier'])
+        self.assertEqual(1, len(invited_game['invitees']))
+        self.assertEqual(invitee, invited_game['invitees'][0]['identifier'])
+        self.assertEqual(GameStatus.WAITING_FOR_PLAYERS.name, invited_game['status'])
+
+    def test_join_public_game(self):
+        '''Any player can join a public game'''
+        player = 'player'
+
+        resp = create_game.main(
+            build_request(
+                body={'name': 'public join test'}))
+        created_game: WaitingGame = read_response_body(resp.get_body())
+
+        resp = join_game.main(
+            build_request(
+                route_params={'game_id': created_game['id']},
+                headers={'x-ms-client-principal-id': player}))
+        joined_game: WaitingGame = read_response_body(resp.get_body())
+
+        self.assertEqual(created_game['id'], joined_game['id'])
+        self.assertEqual(1, len(joined_game['players']))
+        self.assertEqual(player, joined_game['players'][0]['identifier'])
+        self.assertEqual(0, len(joined_game['invitees']))
+        self.assertEqual(GameStatus.WAITING_FOR_PLAYERS.name, joined_game['status'])
+
+    def test_join_private_game_uninvited(self):
+        '''Uninvited players cannot join a private game'''
+        player = 'player'
+
+        resp = create_game.main(
+            build_request(
+                body={'name': 'public join test', 'accessibility': 'PRIVATE'}))
+        created_game: WaitingGame = read_response_body(resp.get_body())
+
+        resp = join_game.main(
+            build_request(
+                route_params={'game_id': created_game['id']},
+                headers={'x-ms-client-principal-id': player}))
+        self.assertEqual(400, resp.status_code)
+
+    def test_join_private_game_invited(self):
+        '''Invited players can join a private game'''
+        player = 'player'
+
+        resp = create_game.main(
+            build_request(
+                body={'name': 'public join test', 'accessibility': 'PRIVATE'}))
+        created_game: WaitingGame = read_response_body(resp.get_body())
+
+        resp = invite_to_game.main(
+            build_request(
+                route_params={'game_id': created_game['id']},
+                headers={'x-ms-client-principal-id': created_game['organizer']['identifier']},
+                body={'invitees': [player]}))
+
+        resp = join_game.main(
+            build_request(
+                route_params={'game_id': created_game['id']},
+                headers={'x-ms-client-principal-id': player}))
+
+        joined_game: WaitingGame = read_response_body(resp.get_body())
+
+        self.assertEqual(created_game['id'], joined_game['id'])
+        self.assertEqual(1, len(joined_game['players']))
+        self.assertEqual(player, joined_game['players'][0]['identifier'])
+        self.assertEqual(0, len(joined_game['invitees']))
+        self.assertEqual(GameStatus.WAITING_FOR_PLAYERS.name, joined_game['status'])
