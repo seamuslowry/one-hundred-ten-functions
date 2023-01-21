7ef0c01685d2bcf02018fb465e1348cd39ee476e
Seamus Lowry <spl1owry3@gmail.com>
2023-01-21 11:59:46 -0500

    fine

--- app/parsers/request.py
+++ app/parsers/request.py
@@ -15,6 +15,6 @@ def parse(req: func.HttpRequest) -> Tuple[User, Game]:
     Parse the request for the models
     '''
     game_id = req.route_params.get('game_id', None)
-    game = GameService.get(game_id or '')
+    game = GameService.get(game_id) if game_id else Game()
 
     return (UserService.save(deserialize.user(req)), game)
